71454cda8633596a5ca154940040ba119ed9e8fe
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2020-01-11 06:13:13 +0300

    refacotor gsub(x, ...) with params()

--- R/get_vis_owner.R
+++ R/get_vis_owner.R
@@ -3,23 +3,24 @@
 #' This function gets the owner of a visualization. it's used in the
 #' \code{dash_builders} to extract a list of dashboard collaborators
 #'
-#' @param visual A data frame with visualization ids.
-#' @param type A character vector indicating the type of visualization. Example;
+#' @param visual a data frame with visualization ids.
+#' @param type a character vector indicating the type of visualization. Example;
 #'   maps, charts or reportTables.
-#' @param server A character vector specifying the server URL.
+#' @param baseurl a character vector specifying a URL of the server.
 #' @param ... Additional parameters passed to get_resources API.
 #' @return A list of user names and ids.
 #' @export
-get_vis_owner <- function(visual, type, server){
+get_vis_owner <- function(visual, type = NULL, baseurl){
+
+
+  #gsub(" ","",toString(vis_ids(visual)))
 
   if (has_multiple_ids(visual)){
-
-    query <- paste0("filter=id:in:[", gsub(" ","",toString(vis_ids(visual))), "]&fields=user[name,id]")
+    query <- paste0("filter=id:in:[", params(vis_ids(visual)), "]&fields=user[name,id]")
     ids <- NULL
 
     if (type == "maps"){
-
-      d <- get_resources(server, type = "maps", id = ids, query = query)
+      d <- get_resources(baseurl, type = "maps", id = ids, query = query)
       user <- unique(d$content$maps$user)
 
 
@@ -27,14 +28,14 @@ get_vis_owner <- function(visual, type, server){
 
     if (type == "charts"){
 
-      d <- get_resources(server, type = "charts",id = ids, query = query)
+      d <- get_resources(baseurl, type = "charts",id = ids, query = query)
       user <- unique(d$content$charts$user)
 
     }
 
     if (type == "reportTables"){
 
-      d <- get_resources(server, type = "reportTables", id = ids, query = query)
+      d <- get_resources(baseurl, type = "reportTables", id = ids, query = query)
       user <- unique(d$content$reportTables$user)
 
     }
@@ -48,21 +49,21 @@ get_vis_owner <- function(visual, type, server){
 
     if (type == "maps"){
 
-      d <- get_resources(server, type = "maps", id = ids, query = query)
+      d <- get_resources(baseurl, type = "maps", id = ids, query = query)
       user <- d$content$user
 
     }
 
     if (type == "charts"){
 
-      d <- get_resources(server, type = "charts",id = ids, query = query)
+      d <- get_resources(baseurl, type = "charts",id = ids, query = query)
       user <- d$content$user
 
     }
 
     if (type == "reportTables"){
 
-      d <- get_resources(server, type = "reportTables", id = ids, query = query)
+      d <- get_resources(baseurl, type = "reportTables", id = ids, query = query)
       user <- d$content$user
 
     }
