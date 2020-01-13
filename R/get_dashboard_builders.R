29d50efa9cd40f601c8e92a929092ffcb6953b58
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2020-01-13 17:27:55 +0300

    code refactoring

--- R/get_dashboard_builders.R
+++ R/get_dashboard_builders.R
@@ -1,9 +1,8 @@
 #' Get users developing a dashboard
 #'
-#' This function provides a list of users working on a dashboard. This includes;
+#' This function provides a list of users working on a common dashboard. This includes;
 #' collaborators and dashboard owners.
 #'
-#' @importFrom magrittr %>%
 #' @param id character, the id of the dashboard
 #' @param baseurl character, baseurl of the string
 #' @param ... Additional parameters passed to \code{get_resource}
@@ -13,12 +12,9 @@ get_dash_builders <- function(id = NULL, baseurl = "", ...){
 
   # collaborators
   collab <- list(
-    maps = parsed$content$dashboardItems$map %>%
-      get_vis_owner(., type = "maps", baseurl),
-    charts = parsed$content$dashboardItems$chart %>%
-      get_vis_owner(., type = "charts", baseurl),
-    tables = parsed$content$dashboardItems$reportTable %>%
-      get_vis_owner(., type = "reportTables", baseurl)
+    maps = get_vis_owner(parsed$content$dashboardItems$map, type = "maps", baseurl),
+    charts = get_vis_owner(parsed$content$dashboardItems$chart, type = "charts", baseurl),
+    tables =  get_vis_owner(parsed$content$dashboardItems$reportTable, type = "reportTables", baseurl)
   )
 
   structure(
